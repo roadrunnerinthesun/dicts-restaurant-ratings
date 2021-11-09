0d1110049c62257009744d955506474c4f02f1e5
Sahara <saharabegom@gmail.com>
2021-11-09 00:44:22 +0000

    Working on Functions

--- ratings.py
+++ ratings.py
@@ -1,4 +1,23 @@
 """Restaurant rating lister."""
 
+# define a function where we pass a file as parameter
+    # open text file
+    # iterate through each line
+        # strip empty space at end of line
+                # Split words in each line
 
-# put your code here
+
+def rating_list(file):
+
+    opened_file = open(file)
+
+    r_ratings = {}
+
+    for line in opened_file:
+        line = line.rstrip()
+        word = line.split(":")
+        print(word)
+hello
+rating_list("scores.txt")
+
+ 
